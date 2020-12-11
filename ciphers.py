6749fcf31a82b555acc22e8be8eae2e00cf6e945
Ben Whitmore <bhwhitmore@gmail.com>
2020-12-11 12:21:57 +0000

    Created substitution cipher and started to implement v1 vs v2.

--- ciphers.py
+++ ciphers.py
@@ -6,20 +6,29 @@ Websites used for research:
 
 """
 Basic ciphers implemented with encrypt and decrypt functions.
-Each function returns either the plain text or cipher text
-Also has a basic functions to allow repeated call for user input.
+Each cipher function returns either the plain text or cipher text.
 
-key = None implies cipher doen NOT require a key or the key is pre set
+Notes:
+	key = None implies cipher doen NOT require a key or the key is pre set.
+	v1 changes only letters and keeps captilization and punctuation, v2 changes everything.
 """
 
 """
-IDEAS:
-	- Have V1 to only change alpha characters and V2 to change all characters
+To Do:
+	- Test every cipher after new update_txt function
 	- Create GUI to display all functions, website?
 """
 
-#Functions and Variables used across the ciphers
+#Functions, Variables & Imports required
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+#Checks if char is upper or lower and updates accordingly
+def update_txt(char, new_char):
+	if char.isupper():
+		new_char = new_char.upper()
+	else:
+		new_char = new_char.lower()
+	return new_char
 
 #Required for Hill Cipher
 def matrix_multiply(a, b):
@@ -36,21 +45,18 @@ Affine Cipher
 	Uses a mathmatical calculation to create the new char value for the cipher text
 	Key type is 2x +ve integers
 """
-def affine_encryption(txt, key_a, key_b):
+def affine_encryption_v1(txt, key_a, key_b):
 	cipher_txt = ""
 	for char in txt:
 		if char.isalpha():
 			p = alphabet.find(char.lower())
 			c = ((key_a * p) + key_b) % 26
-			if char.isupper():
-				cipher_txt += alphabet[c].upper()
-			else:
-				cipher_txt += alphabet[c].lower()
+			cipher_txt += update_txt(char, alphabet[c])
 		else:
 			cipher_txt += char
 	return cipher_txt
 
-def affine_decryption(txt, key_a, key_b):
+def affine_decryption_v1(txt, key_a, key_b):
 	plain_txt = ""
 	#Gets the inverse of key_a
 	for x in range (1, 25):
@@ -62,10 +68,7 @@ def affine_decryption(txt, key_a, key_b):
 		if char.isalpha():
 			c = alphabet.find(char.lower())
 			p = inv_a * (c - key_b) % 26
-			if char.isupper():
-				plain_txt += alphabet[p].upper()
-			else:
-				plain_txt += alphabet[p].lower()
+			plain_txt += update_txt(char, alphabet[p])
 		else:
 			plain_txt += char
 	return plain_txt
@@ -75,14 +78,36 @@ Atbash Cipher
 	The substitution cipher with a set key, which is the character set reversed
 	Key type is a pre-set list
 """
-def atbash_encrypt(txt, key=None):
+def atbash_encrypt_v1(txt, key=None):
+	cipher_txt = ""
+	for char in txt:
+		if char.isalpha():
+			position = alphabet.find(char.lower())
+			ascii_val = (25 - position)
+			cipher_txt += update_txt(char, alphabet[ascii_val])
+		else:
+			cipher_txt += char
+	return cipher_txt
+
+def atbash_decrypt_v1(txt, key=None):
+	plain_txt = ""
+	for char in txt:
+		if char.isalpha():
+			position = alphabet.find(char.lower())
+			ascii_val = (25 - position)
+			plain_txt += update_txt(char, alphabet[ascii_val])
+		else:
+			plain_txt += char
+	return plain_txt
+
+def atbash_encrypt_v2(txt, key=None):
 	cipher_txt = ""
 	for char in txt:
 		ascii_val = (127 - ord(char))
 		cipher_txt += chr(ascii_val)
 	return cipher_txt
 
-def atbash_decrypt(txt, key=None):
+def atbash_decrypt_v2(txt, key=None):
 	plain_txt = ""
 	for char in txt:
 		ascii_val = (127 - ord(char))
@@ -94,22 +119,19 @@ Baconian Cipher
 	The substitution cipher with a specific key, each character bounded to multple letters
 	Key type is a pre set list
 """
-def baconian_encrypt(txt, key=None):
+def baconian_encrypt_v1(txt, key=None):
 	cipher_txt = ""
 	baconian_key = ["aaaaa", "aaaab", "aaaba", "aaabb", "aabaa", "aabab", "aabba", "aabbb", "abaaa", "abaaa", "abaab", "ababa", "ababb", "abbaa", "abbab", "abbba", "abbbb", "baaaa", "baaab", "baaba", "baabb", "baabb", "babaa", "babab", "babba", "babbb"] #Note I/J = abaaa and U/V = baabb
 	for char in txt:
 		if char.isalpha():
 			val = alphabet.find(char.lower())
-			if char.isupper():
-				cipher_txt += baconian_key[val].upper()
-			else:
-				cipher_txt += baconian_key[val].lower()
+			cipher_txt += update_txt(char, baconian_key[val])
 		else:
 			cipher_txt += char
 	return cipher_txt
 
-def baconian_decrypt(txt, key=None):
-	cipher_txt = ""
+def baconian_decrypt_v1(txt, key=None):
+	plain_txt = ""
 	baconian_key = ["aaaaa", "aaaab", "aaaba", "aaabb", "aabaa", "aabab", "aabba", "aabbb", "abaaa", "abaaa", "abaab", "ababa", "ababb", "abbaa", "abbab", "abbba", "abbbb", "baaaa", "baaab", "baaba", "baabb", "baabb", "babaa", "babab", "babba", "babbb"] #Note I/J = abaaa and U/V = baabb
 	count, temp_word = 0, ""
 	for char in txt:
@@ -118,28 +140,47 @@ def baconian_decrypt(txt, key=None):
 			temp_word += char
 			if count == 5:
 				val = baconian_key.index(temp_word.lower())
-				if temp_word[0].isupper():
-					cipher_txt += alphabet[val].upper()
-				else:
-					cipher_txt += alphabet[val].lower()
+				plain_txt += update_txt(char, baconian_key[val])
 				count, temp_word = 0, ""
 		else:
-			cipher_txt += char
-	return cipher_txt
+			plain_txt += char
+	return plain_txt
 
 """
 Caesar Cipher
 	Shifts each character over by the value of the key
 	Key type is an integer
 """
-def caesar_encrypt(txt, key):
+def caesar_encrypt_v1(txt, key):
+	cipher_txt = ""
+	for char in txt:
+		if char.isalpha():
+			position = alphabet.find(char.lower())
+			ascii_val = (position + key) % 26
+			cipher_txt += update_txt(char, alphabet[ascii_val])
+		else:
+			cipher_txt += char
+	return cipher_txt
+
+def caesar_decrypt_v1(txt, key):
+	plain_txt = ""
+	for char in txt:
+		if char.isalpha():
+			position = alphabet.find(char.lower())
+			ascii_val = (position - key) % 26
+			plain_txt += update_txt(char, alphabet[ascii_val])
+		else:
+			plain_txt += char
+	return plain_txt
+
+def caesar_encrypt_v2(txt, key):
 	cipher_txt = ""
 	for char in txt:
 		ascii_val = (ord(char) - 32 + key) % 94 + 32
 		cipher_txt += chr(ascii_val)
 	return cipher_txt
 
-def caesar_decrypt(txt, key):
+def caesar_decrypt_v2(txt, key):
 	plain_txt = ""
 	for char in txt:
 		ascii_val = (ord(char) - 32 - key) % 94 + 32
@@ -152,7 +193,7 @@ Hill Cipher
 	Key type is a 3x3 matrix, 2d array of 3 lists containing 3 integers
 """
 
-def hill_encrypt(txt, key):
+def hill_encrypt_v1(txt, key):
 	cipher_txt = ""
 	char_count, temp_lst, none_alpha_dict = 0, [], {}
 	while char_count < len(txt):
@@ -170,6 +211,7 @@ def hill_encrypt(txt, key):
 					for dict_key in none_alpha_dict:
 						temp_char = none_alpha_dict[dict_key]
 						position, char_value = temp_char[0], temp_char[1]
+						char_value = update_txt(char, char_value)
 						cipher_txt = cipher_txt[:position] + char_value + cipher_txt[position:]
 				temp_lst, none_alpha_dict = [], {}
 				char_count -= 1 #To stop it skiping the 4th char
@@ -200,7 +242,7 @@ def hill_encrypt(txt, key):
 	return cipher_txt
 
 #---------------------- NOT DONE YET ----------------------
-def hill_decrypt(txt, key):
+def hill_decrypt_v1(txt, key):
 	pass
 
 """
@@ -208,21 +250,18 @@ Key Phrase Cipher
 	A 26 letter key is used to replace plain text with the key letter assigned to the alphabet
 	Key type is a 26 character string
 """
-def key_phrase_encrypt(txt, key):
+def key_phrase_encrypt_v1(txt, key):
 	cipher_txt = ""
 	for char in txt:
 		if char.isalpha():
 			val = alphabet.find(char.lower())
-			if char.isupper():
-				cipher_txt += key[val].upper()
-			else:
-				cipher_txt += key[val].lower()
+			cipher_txt += update_txt(char, key[val])
 		else:
 			cipher_txt += char
 	return cipher_txt 
 
 #---------------------- NOT DONE YET ----------------------
-def key_phrase_decrypt(txt, key):
+def key_phrase_decrypt_v1(txt, key):
 	pass
 	#going to have to use stats to decrypt since not one-one
 
@@ -232,7 +271,7 @@ Morse Code
 	The key is a pre defined dict of morse code values
 	Note: / is used to seperate the words for the user
 """
-def txt_to_morse(txt, key=None):
+def txt_to_morse_v1(txt, key=None):
 	morse_txt = ""
 	key = {
 		"a": ".-", "b": "-...", "c": "-.-.",
@@ -254,7 +293,7 @@ def txt_to_morse(txt, key=None):
 			morse_txt += char
 	return morse_txt
 
-def morse_to_txt(txt, key=None):
+def morse_to_txt_v1(txt, key=None):
 	plain_txt = ""
 	key = {
 		".-": "a", "-...": "b", "-.-.": "c",
@@ -294,7 +333,7 @@ Rail-Fence Cipher
 	Writes each letter in a zig-zag (based on key val) then merges lines top to bottom
 	Key type is an integer
 """
-def rail_fence_encrypt(txt, key):
+def rail_fence_encrypt_v2(txt, key):
 	pass_val = 0
 	temp_dict = {}
 	cipher_txt = ""
@@ -317,7 +356,7 @@ def rail_fence_encrypt(txt, key):
 	return cipher_txt
 
 #---------------------- NOT DONE YET ----------------------
-def rail_fence_decrypt(txt, key):
+def rail_fence_decrypt_v2(txt, key):
 	pass
 
 """
@@ -325,14 +364,36 @@ ROT13 Cipher
 	The ceaser cipher with a key set to 13
 	Key type is an pre-set integer
 """
-def rot13_encrypt(txt, key=None):
+def rot13_encrypt_v1(txt, key=None):
+	cipher_txt = ""
+	for char in txt:
+		if char.isalpha():
+			position = alphabet.find(char.lower())
+			ascii_val = (position + 13) % 26
+			cipher_txt += update_txt(char, alphabet[ascii_val])
+		else:
+			cipher_txt += char
+	return cipher_txt
+
+def rot13_decrypt_v1(txt, key=None):
+	plain_txt = ""
+	for char in txt:
+		if char.isalpha():
+			position = alphabet.find(char.lower())
+			ascii_val = (position - 13) % 26
+			plain_txt += update_txt(char, alphabet[ascii_val])
+		else:
+			plain_txt += char
+	return plain_txt
+
+def rot13_encrypt_v2(txt, key=None):
 	cipher_txt = ""
 	for char in txt:
 		ascii_val = (ord(char) - 32 + 13) % 94 + 32
 		cipher_txt += chr(ascii_val)
 	return cipher_txt
 
-def rot13_decrypt(txt, key=None):
+def rot13_decrypt_v2(txt, key=None):
 	plain_txt = ""
 	for char in txt:
 		ascii_val = (ord(char) - 32 - 13) % 94 + 32
@@ -345,15 +406,30 @@ Substitution Cipher
 	Key type is a list
 """
 #---------------------- NOT DONE YET ----------------------
-def substitution_encrypt(txt, key):
-	pass
+def substitution_encrypt_v1(txt, key):
+	cipher_txt = ""
+	for char in txt:
+		if char.isalpha():
+			position = alphabet.find(char.lower())
+			cipher_txt += update_txt(char, key[position])
+		else: 
+			cipher_txt += char
+	return cipher_txt
 
-def substitution_decrypt(txt, key):
-	pass
+def substitution_decrypt_v1(txt, key):
+	plain_txt = ""
+	for char in txt:
+		if char.isalpha():
+			position = key.index(char.lower())
+			plain_txt += update_txt(char, alphabet[position])
+		else: 
+			plain_txt += char
+	return plain_txt
 
 #--- USED TO TEST ---	
 test = "Hello, this is a test example!"
-e = affine_encryption(test, 15, 11)
+alphabet_lst = ["g", "h", "i", "l", "m", "n", "q", "r", "s", "t", "u", "v", "w", "o", "p", "a", "b", "c", "j", "k", "d", "e", "f", "x", "y", "z"]
+e = substitution_encrypt_v1(test, alphabet_lst)
 print(e)
-d = affine_decryption(e, 15, 11)
+d = substitution_decrypt_v1(e, alphabet_lst)
 print(d)
